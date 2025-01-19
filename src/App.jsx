829ad058e80844a514f0ca76b7c855b9237e6cba
Khaled <khaledlabib_7@hotmail.com>
2025-01-19 18:56:49 +0200

    fourth commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
-import {
-  createBrowserRouter,
-  HashRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
 import Home from "./pages/Home/Home";
@@ -27,43 +24,6 @@ import VerifyCode from "./pages/VerifyCode/VerifyCode";
 import ResetPassword from "./pages/ResetPassword/ResetPassword";
 
 function App() {
-  const router = HashRouter([
-    {
-      path: "/",
-      element: (
-        <ProtectedRoute>
-          <Layout />
-        </ProtectedRoute>
-      ),
-      children: [
-        { index: true, element: <Home /> },
-        { path: "cart", element: <Cart /> },
-        { path: "product/:id", element: <ProductDetails /> },
-        { path: "checkout", element: <Checkout /> },
-        { path: "allorders", element: <Orders /> },
-        { path: "categories", element: <Categories /> },
-        { path: "brands", element: <Brands /> },
-        { path: "wishlist", element: <Wishlist /> },
-      ],
-    },
-
-    {
-      path: "/",
-      element: (
-        <GuestRoute>
-          <Layout />
-        </GuestRoute>
-      ),
-      children: [
-        { path: "signup", element: <Signup /> },
-        { path: "login", element: <Login /> },
-        { path: "forgetpassword", element: <ForgetPassword /> },
-        { path: "verifycode", element: <VerifyCode /> },
-        { path: "resetpassword", element: <ResetPassword /> },
-      ],
-    },
-  ]);
-
   // const router = createBrowserRouter([
   //   {
   //     path: "/",
@@ -108,7 +68,43 @@ function App() {
       <QueryClientProvider client={myClient}>
         <UserProvider>
           <CartProvider>
-            <RouterProvider router={router} />
+            {/* <RouterProvider router={router} /> */}
+            <Router>
+              <Routes>
+                <Route
+                  path="/"
+                  element={
+                    <ProtectedRoute>
+                      <Layout />
+                    </ProtectedRoute>
+                  }
+                >
+                  <Route index element={<Home />} />
+                  <Route path="cart" element={<Cart />} />
+                  <Route path="product/:id" element={<ProductDetails />} />
+                  <Route path="checkout" element={<Checkout />} />
+                  <Route path="allorders" element={<Orders />} />
+                  <Route path="categories" element={<Categories />} />
+                  <Route path="brands" element={<Brands />} />
+                  <Route path="wishlist" element={<Wishlist />} />
+                </Route>
+
+                <Route
+                  path="/"
+                  element={
+                    <GuestRoute>
+                      <Layout />
+                    </GuestRoute>
+                  }
+                >
+                  <Route path="signup" element={<Signup />} />
+                  <Route path="login" element={<Login />} />
+                  <Route path="forgetpassword" element={<ForgetPassword />} />
+                  <Route path="verifycode" element={<VerifyCode />} />
+                  <Route path="resetpassword" element={<ResetPassword />} />
+                </Route>
+              </Routes>
+            </Router>
           </CartProvider>
         </UserProvider>
 
